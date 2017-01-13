dc4d519d5a7a519b5cff5ccec79ffc769c3d6450
boertel <benjamin.oertel@gmail.com>
2017-01-12 18:47:11 -0800

    add brew commands

---
+++ brew.sh
@@ -0,0 +1,15 @@
+brew update
+brew upgrade
+
+brew install neovim/neovim/neovim
+
+brew tap neovim/neovim
+brew tap rogual/neovim-dot-app
+brew install neovim-dot-app
+brew linkapps neovim-dot-app
+
+brew tap caskroom/cask
+brew tap boertel/tap
+
+brew install bump
+brew install vasy
