61f0b219a67ea0a1a4f8c9e47b24e249b6b2d62f
boertel <benjamin.oertel@gmail.com>
2024-04-29 20:57:57 -0700

    update cask

--- homebrew/cask.sh
+++ homebrew/cask.sh
@@ -3,21 +3,32 @@ brew install --cask slack
 brew install --cask spectacle
 brew install --cask flux
 brew install --cask spotify
-brew install --cask google-chrome
 brew install --cask firefox
-brew install --cask hammerspoon
-brew install --cask alfred
-brew install --cask anybar
+brew install --cask arc
 brew install --cask chromium
 brew install --cask figma
+brew install --cask raycast
+brew install --cask alacritty
 
-brew install --cask disk-inventory-x
-brew install --cask dafuqtor/tap/midclick
+brew install --cask mimestream
+brew install --cask omnidisksweeper
 brew install --cask keycastr
-brew install --cask kap
 brew install --cask signal
 brew install --cask whatsapp
-brew install --cask transmission
 brew install --cask visual-studio-code
 brew install --cask vlc
 brew install --cask zoom
+brew install --cask 1password
+brew install --cask monitorcontrol
+brew install --cask karabiner-elements
+brew install --cask devcleaner
+brew install --cask appcleaner
+
+# brew install --cask disk-inventory-x
+# brew install --cask dafuqtor/tap/midclick
+# brew install --cask google-chrome
+# brew install --cask hammerspoon
+# brew install --cask alfred
+# brew install --cask anybar
+# brew install --cask kap
+# brew install --cask transmission
